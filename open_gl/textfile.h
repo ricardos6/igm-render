0fee9a874b651fd95172d395b8dba360904ec3b3
Ricardo Sanchez <r.sanchezarias6@gmail.com>
2022-01-12 18:52:00 +0100

    esqueleto practica parte 1

---
+++ open_gl/textfile.h
@@ -0,0 +1,23 @@
+// textfile.h: interface for reading and writing text files
+// www.lighthouse3d.com
+//
+// You may use these functions freely.
+// they are provided as is, and no warranties, either implicit,
+// or explicit are given
+//
+// Slightly modified by Emilio J. Padrón
+//////////////////////////////////////////////////////////////////////
+
+// char *textFileRead(const char *fn);
+// int textFileWrite(const char *fn, const char *s);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *textFileRead(const char *fn);
+int textFileWrite(const char *fn, const char *s);
+
+#ifdef __cplusplus
+}
+#endif
